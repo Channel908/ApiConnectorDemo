b58280cc7d160fb338ad4a0c3356b919a613ae9c
RJBDEVW11\brink <brinkley908@gmail.com>
2023-08-28 16:08:44 +0100

    Add project files.

---
+++ ApiConnectorDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiConnectorDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
